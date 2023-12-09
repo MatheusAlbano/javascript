8cf10896d2613de13946b72b919040bf164d4b6f
Matheus Albano Porto <135064012+MatheusAlbano@users.noreply.github.com>
2023-12-09 11:59:44 -0300

    aula 11 js

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel>60) { //CONDIÇÃO SIMPLES
+        console.log('Você ultrapassou o limite de velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
