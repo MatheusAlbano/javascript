3aa336b5c42d20c2b66179cc3cb2fbff345fa873
Matheus Albano Porto <135064012+MatheusAlbano@users.noreply.github.com>
2023-12-10 19:16:46 -0300

    fim do modulo E

---
+++ aula014/ambiente.js
@@ -0,0 +1,20 @@
+for(c=1;c<7;c++){
+    console.log('Tudo bem?')
+}
+
+
+/* 
+var c = 1
+while(c<7){
+    console.log('Tudo bem?')
+    c++
+}
+
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
